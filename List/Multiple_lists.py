ce056802cd2b759eae96c40065e54e49466be178
Antonio Vega <a.vegaramirez76@gmail.com>
2018-05-18 20:10:33 +0900

    Update

--- List/Multiple_lists.py
+++ List/Multiple_lists.py
@@ -1,2 +1,8 @@
 available_tacos = ['pastor', 'bistec', 'chorizo']
-requested_tacos = ['tripa, pastor, arrachera']
+requested_tacos = ['tripa', 'pastor', 'arrachera']
+
+for requested_taco in requested_tacos:
+    if requested_taco in available_tacos:
+        print("Adding taco of " + requested_taco + ".")
+    else:
+        print("Sorry, we don't have " + requested_taco + ".")
