42cbb04f6e7bb480f2c403941fa40d4a41a93bed
Antonio Vega <a.vegaramirez76@gmail.com>
2018-05-18 21:38:52 +0900

    Update

--- Dictionary/Simple_Dictionary.py
+++ Dictionary/Simple_Dictionary.py
@@ -1,3 +1,22 @@
+# Dictionaries always go in pairs, label and value
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+print(alien_0)
+
+# Add a new value
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+# Starting a new empty dictionary
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+# Modify a dictionary
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + ".")
+alien_0['color'] = 'yellow'
+print("The alien is now " + alien_0['color'] + ".")
