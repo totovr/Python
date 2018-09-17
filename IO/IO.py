0d36b1f154fd9aa75b4611d951cf0708043a056c
Antonio Vega <a.vegaramirez76@gmail.com>
2018-09-17 10:41:16 +0900

    IO

---
+++ IO/IO.py
@@ -0,0 +1,11 @@
+# input method
+decimal = float( input("Introduce a decimal with a decimal point: "))
+
+# example
+
+values = []
+print("Introduce 3 values: ")
+for x in range(3):
+    values.append( input("Introduce one value > "))
+
+values
