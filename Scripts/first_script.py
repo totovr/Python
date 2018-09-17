be935b760bb72580099f5f482cd4209103e6bcac
Antonio Vega <a.vegaramirez76@gmail.com>
2018-09-17 11:00:57 +0900

    scripts

---
+++ Scripts/first_script.py
@@ -0,0 +1,4 @@
+import sys
+
+print("Hello, welcome to your first script")
+print(sys.argv)
---
+++ Scripts/write_lines.py
@@ -0,0 +1,7 @@
+import sys
+# Position for text
+text = sys.argv[1]
+# Number of repetitions
+repetitions = int(sys.argv[2])
+for r in range(repetitions):
+    print(text)
