afc0afa4e9bc4a02e82c908ec2c19a7f9b065790
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-25 18:59:10 +0900

    objects_inside_objects

--- OOP/especial_methods.py
+++ OOP/especial_methods.py
@@ -10,15 +10,21 @@ class Movie:
     def __del__(self):
         print("The movie {} was delated ".format(self.title))
 
+    # Redefine string
     def __str__(self):
         return "{} was relased in {} and has a duration of {}".format(self.title, self.launched, self.time)
+
+    # Redefine the method lenght
+    def __len__(self):
+        return self.time
 
 m = Movie("the Good father", 175, 1972)
 
 # To erase the constructor
-del(m)
+# del(m)
 
 m
 
-str(10)
 str(m)
+
+len(m)
---
+++ OOP/objects_inside_objects.py
@@ -0,0 +1,30 @@
+class Movie:
+    # This is the constructor
+    def __init__(self, title, launched):
+        self.title = title
+        self.launched = launched
+        print("The movie '{}' was created".format(self.title))
+
+    # Redefine string
+    def __str__(self):
+        return "{} ({})".format(self.title, self.launched)
+
+class Catalogue:
+    movies = []
+
+    def __init__(self, movies = []):
+        self.movies = movies
+
+    def add_movies(self, p):
+        self.movies.append(p)
+
+    def show(self):
+        for p in self.movies:
+            print(p)
+
+p = Movie("The Goodfather", 1972)
+c = Catalogue([p])
+
+c.show()
+c.add_movies(Movie("The Goodfather: Part 2", 1974))
+c.show()
