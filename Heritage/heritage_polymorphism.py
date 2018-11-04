41a1bf0dc86999be40788a72fe1bdc8087a86213
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-04 13:24:26 +0900

    heritage with polymorphism

--- Heritage/heritage_polymorphism.py
+++ Heritage/heritage_polymorphism.py
@@ -1,19 +1,3 @@
-class Product:
-    def __init__(self, reference, type, name, pfp, description, productor = None, distribuitor = None, isbn = None, author = None):
-        self.reference = reference
-        self.type = type
-        self.name = name
-        self.pfp = pfp
-        self.description = description
-        self.productor = productor
-        self.distribuitor = distribuitor
-        self.isbn = isbn
-        self.author = author
-
-ornament = Product('000A','ornament','glass',15,'glass with draws')
-ornament.type
-
-# Because we have 3 kind of different products we have to define a father class and son class
 # Father class
 class Product:
     def __init__(self, reference, name, pfp, description):
@@ -29,13 +13,11 @@ class Product:
         PfP \t\t{}
         DESCRIPTION \t{}""".format(self.reference,self.name,self.pfp,self.description)
 
+# Son class
+
 class Ornament(Product):
     pass
 
-o = Ornament('000A','Ornament',15,'Glass with cool draws')
-print(o)
-
-# Son class
 class Food(Product):
 
     productor = ""
@@ -49,11 +31,6 @@ class Food(Product):
         DESCRIPTION \t{}
         PRODUCTOR \t{}
         DISTRIBUITOR \t{}""".format(self.reference,self.name,self.pfp,self.description,self.productor,self.distribuitor)
-
-f = Food('000A','Oil',15,'Olive oil')
-f.productor = "The happy oiler"
-f.distribuitor = "Costco"
-print(f)
 
 # Son class
 class Book(Product):
@@ -70,7 +47,55 @@ class Book(Product):
         PRODUCTOR \t{}
         DISTRIBUITOR \t{}""".format(self.reference,self.name,self.pfp,self.description,self.isbn,self.author)
 
+# Elements
+
+o = Ornament(2020,"Glass with pictures",10,"Animal print")
+
+f = Food('000A','Oil',15,'Olive oil')
+f.productor = "The happy oiler"
+f.distribuitor = "Costco"
+
 b = Book('000A','Rich Japanese Food',20,'Cook book')
 b.isbn = "a1029343"
 b.author = "The chefchef"
-print(b)
+
+# List of the products
+products = [o,f]
+products.append(b)
+
+products
+
+for p in products:
+    print(p, "\n")
+
+for p in products:
+    print(p.reference,p.name)
+
+# This is an error because not all the class has the same intances
+for p in products:
+    print(p.author)
+
+# This method is to check if an object if of the type that is evaluating
+for p in products:
+    if( isinstance(p, Ornament) ):
+        print(p.reference,p.name)
+    elif( isinstance(p, Food) ):
+        print(p.reference,p.name,p.productor)
+    elif( isinstance(p, Book) ):
+        print(p.reference,p.name,p.isbn)
+
+def discount_product(p, discount):
+    """ Give discount of a product that is in the list"""
+    p.pfp = p.pfp - (p.pfp/100 * discount)
+    return p
+
+p_discount = discount_product(f, 10)
+print(p_discount)
+
+# Method to copy one object
+import copy
+
+copy_o = copy.copy(o)
+print(o)
+copy_o.pfp = 15
+print(copy_o)
