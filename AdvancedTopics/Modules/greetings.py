02090ab58bb6da6a8b1262b2d4c0d1f7b354ee91
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-13 22:59:27 +0900

    modules

---
+++ AdvancedTopics/Modules/greetings.py
@@ -0,0 +1,7 @@
+# This is a module with greeting functions
+def greeting():
+  print("Hello, I'm greeting you from function greeting of the module greetings")
+
+class Greeting():
+    def __init__(self):
+        print("Hello, I'm greeting you from the Class greetings")
---
+++ AdvancedTopics/Modules/test/test.py
@@ -0,0 +1,24 @@
+# If we just want to import one function we can write
+# from greetings import greeting
+# greeting()
+
+# if we want to import all the functions we have to write
+#from greetings import *
+#greeting()
+
+# With this we will import just the class
+#from greetings import Greeting
+
+#Greeting()
+
+# Import a module that is located in a different path
+# To import the module that we create in the sys path
+import sys
+
+sys.path.insert(1, '..')
+# print(sys.path)
+
+# With this we will import just the class
+from greetings import Greeting
+
+Greeting()
