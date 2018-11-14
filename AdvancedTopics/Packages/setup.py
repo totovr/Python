fa5932ecc539cc91a4dfae15e6fcafb3b42bd528
totovr <a.vegaramirez76@gmail.com>
2018-11-14 12:18:16 +0900

    package working

---
+++ AdvancedTopics/Packages/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="tonopackage",
+    version="0.1",
+    description="This is a package example",
+    author="Tono Vega",
+    author_email="a.vegaramirez76@gmail.com",
+    url="https://totovr.github.io/",
+    scripts=[],
+    packages=["package", "package.bye", "package.hello"]
+)
