4bbad04a1f384099cac04e94c76f3e822c67ea41
totovr <a.vegaramirez76@gmail.com>
2018-11-18 19:29:32 +0900

    characters load not repeated

--- AdvancedTopics/FileManagment/ExtraExamples/characters/characters.py
+++ AdvancedTopics/FileManagment/ExtraExamples/characters/characters.py
@@ -3,53 +3,53 @@ import pickle
 
 class Character:
     
-    def __init__(self, cType, cLife, cAttack, cDefense, cRange):
-        self.cType = cType
-        self.cLife = cLife
-        self.cAttack = cAttack
-        self.cDefense = cDefense
-        self.cRange = cRange
-    
+    def __init__(self, cT, cL, cA, cD, cR):
+        self.cT = cT
+        self.cL = cL
+        self.cA = cA
+        self.cD = cD
+        self.cR = cR
+
     def __str__(self):
-        return "The {} has {} of life, {} of attack, {} of defense and {} of range".format(
-            self.cType, self.cLife, self.cAttack, self.cDefense, self.cRange)
+        return "The {} has {} points of life, {} of attack, {} of defense and {} of range ".format(
+        self.cT, self.cL, self.cA, self.cD, self.cR)
 
 class Manager:
     
-    charactersL = []
+    characterL = []
+
     def __init__(self):
         self.load()
-    
-    # Load the file 
+
+    def add_character(self, p):
+
+        for cTemp in self.characterL:
+            if cTemp.cT == p.cT:
+                return
+        self.characterL.append(p)
+        self.save()
+
+    def show(self):
+        if len(self.characterL) == 0:
+            print("There are not characters in the game")
+        else:
+            for p in self.characterL:
+                print(p)
+
     def load(self):
-        file = open('catalogue.pckl', 'ab+') # binary append with lecture mode
+        file = open('game.pckl', 'ab+') # binary append with lecture mode
         file.seek(0)
         try:
-            self.charactersL = pickle.load(file)
+            self.characterL = pickle.load(file)
         except:
             print("This file is empty") # if the file is empty 
         finally:
             file.close()
-            print("{} characters were loaded".format(len(self.charactersL)))
-
-    def add_characters(self, p):
-        for characterTemp in self.charactersL: # loop all the character list
-            if characterTemp.Type == p.Type: # check if the type is in the object list
-                return 
-        # If the character is not in the list it will be add
-        self.charactersL.append(p)
-        self.save()
-
-    def show(self):
-        if len(self.charactersL) == 0:
-            print("There are not characters in the catalogue")
-        else:
-            for p in self.charactersL:
-                print(p)
+            print("{} characters were loaded".format(len(self.characterL)))
     
     def save(self):
-        file = open('catalogue.pckl', 'wb+')
-        pickle.dump(self.charactersL, file)
+        file = open('game.pckl', 'wb+')
+        pickle.dump(self.characterL, file)
         file.close()
 
     # Destroy the class
@@ -57,3 +57,14 @@ class Manager:
         self.save() # automatic save
         print("The catalogue was saved")
 
+# Create the manager
+c = Manager()
+# show the content
+c.show()
+# Add the movies
+c.add_character( Character("Caballero",4,2,4,2) )
+c.add_character( Character("Caballero",4,2,4,2) )
+# Show again the catalogue
+c.show()
+# Delate the catalogue of the ram memory, the file will persist in the folder
+del(c)
